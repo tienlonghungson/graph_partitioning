d208ee6e8d3ba9bcc336b5dff8d95e6271dfd538
tienlonghungson <nguyentienlong0203hungson@gmail.com>
2021-12-14 09:42:25 +0700

    add sparse args

--- src/main/Config.java
+++ src/main/Config.java
@@ -3,8 +3,8 @@ package main;
 import service.Pair;
 
 public class Config {
-    public static int[][] kArray = new int[][] {{2,3,4},{4,5,6},{6,7,8}};
-//    public static int[][] kArray = new int[][] {{2},{4,5,6},{6,7,8}};
+//    public static int[][] kArray = new int[][] {{2,3,4},{4,5,6},{6,7,8}};
+    public static int[][] kArray = new int[][] {{2},{4,5,6},{6,7,8}};
     public static int[][] alphaArray = new int[][] {{1,2},{5,6},{10,11}};
 
     public static Pair<int[],int[]> getParameter(String dataSize){
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,8 +6,10 @@ import model.evolution.nsgaii.NSGA2;
 import model.integerprogramming.IPModel;
 import model.tabu.TabuModel;
 import service.Pair;
+import service.Triplet;
 
 import java.io.IOException;
+import java.util.SplittableRandom;
 
 public class Main {
 
@@ -16,12 +18,45 @@ public class Main {
     }
 
     public static void main(String[] args){
-        // args[0]:nRun, args[1]:data_size
-        int nRun = Integer.parseInt(args[0]);
-        Pair<int[],int[]> problemParameter = Config.getParameter(args[1]);
-        AbstractModel.execute(new TabuModel(),nRun,problemParameter.first(), problemParameter.second(),args[1]);
-        AbstractModel.execute(new IPModel(),1,problemParameter.first(), problemParameter.second(),args[1]);
-        AbstractModel.execute(new CPModel(),1,problemParameter.first(), problemParameter.second(),args[1]);
+
+        // args[0]: modelName, args[1]:nRun, args[2]:data_size
+        Triplet<AbstractModel, Integer, String> parsedArgs = parseArgument(args);
+        if (parsedArgs==null) {
+            return;
+        }
+        Pair<int[],int[]> problemParameter = Config.getParameter(args[2]);
+        AbstractModel.execute(parsedArgs.first(),parsedArgs.second(), problemParameter.first(), problemParameter.second(),args[2]);
+
+
+//        switch (args[0]){
+//            case "IP" : AbstractModel.execute(new IPModel(),1,problemParameter.first(), problemParameter.second(),args[2]);
+//            case "CP" : AbstractModel.execute(new CPModel(),1,problemParameter.first(), problemParameter.second(),args[2]);
+//            case "TabuSearch" : AbstractModel.execute(new TabuModel(),nRun,problemParameter.first(), problemParameter.second(),args[1]);
+//        }
+////        AbstractModel.execute(new TabuModel(),nRun,problemParameter.first(), problemParameter.second(),args[1]);
+//        AbstractModel.execute(new IPModel(),1,problemParameter.first(), problemParameter.second(),args[1]);
+//        AbstractModel.execute(new CPModel(),1,problemParameter.first(), problemParameter.second(),args[1]);
 //        AbstractModel.execute(new NSGA2(),1,Config.kArray[0], Config.alphaArray[0],"test");
+    }
+
+    private static Triplet<AbstractModel,Integer,String> parseArgument(String[] args){
+        if (args.length<3) {
+            System.out.println("Not Enough Parameter");
+            return null;
+        }
+
+        int nRun;
+        try {
+            nRun = Integer.parseInt(args[1]);
+        } catch (NumberFormatException numberFormatException){
+            nRun=1;
+        }
+
+        return switch (args[0]) {
+            case "CP" -> new Triplet<>(new CPModel(), 1, args[2]);
+            case "TabuSearch" -> new Triplet<>(new TabuModel(), nRun, args[2]);
+            case "NSGA2" -> new Triplet<>(new NSGA2(), nRun, args[2]);
+            default -> new Triplet<>(new IPModel(), 1, args[2]);
+        };
     }
 }
